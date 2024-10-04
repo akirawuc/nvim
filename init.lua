a654180b3b3379a7be6b44db0fb6c68b9f80242b
akirawuc <stu970029@gmail.com>
2024-10-04 15:11:25 +0800

    feat: upgrade to using init.lua

---
+++ init.lua
@@ -0,0 +1,133 @@
+-- Initialize the plugin manager
+vim.cmd [[packadd packer.nvim]]
+
+require('packer').startup(function()
+  use 'wbthomason/packer.nvim' -- Ensure packer itself is managed
+
+  -- Core plugins
+  use 'tpope/vim-sensible'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-commentary'
+  use 'tpope/vim-abolish'
+  use 'github/copilot.vim'
+  use 'nvim-lua/plenary.nvim'
+
+  -- Appearance
+  use 'ryanoasis/vim-devicons'
+  use 'kyazdani42/nvim-web-devicons'
+  use 'vim-airline/vim-airline'
+  use 'vim-airline/vim-airline-themes'
+  use 'morhetz/gruvbox'
+  use { 'hachy/eva01.vim', branch = 'main' }
+
+  -- Useful tools
+  use '907th/vim-auto-save'
+  use 'preservim/nerdtree' -- Updated NERDTree repo
+  use 'easymotion/vim-easymotion'
+  use 'voldikss/vim-floaterm'
+  use 'mhinz/vim-startify'
+  use 'ray-x/lsp_signature.nvim'
+  use { 'neoclide/coc.nvim', branch = 'release' }
+  use 'folke/trouble.nvim'
+  use 'akinsho/toggleterm.nvim'
+
+  -- Rust support
+  use 'rust-lang/rust.vim'
+
+  -- JS and frontend support
+  use 'neoclide/jsonc.vim'
+  use 'othree/html5.vim'
+  use 'pangloss/vim-javascript'
+  use 'MaxMEllon/vim-jsx-pretty'
+  use { 'styled-components/vim-styled-components', branch = 'main' }
+  use 'leafgarland/typescript-vim'
+  use 'peitalin/vim-jsx-typescript'
+  use 'TovarishFin/vim-solidity'
+end)
+
+-- General settings
+vim.o.encoding = 'utf-8'
+vim.g.fileencoding = 'utf-8'
+vim.o.background = 'dark'
+vim.o.nocompatible = true
+vim.o.showmatch = true
+vim.o.ignorecase = true
+vim.o.mouse = 'a'
+vim.o.hlsearch = true
+vim.o.incsearch = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.autoindent = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cc = "80"
+vim.o.syntax = 'on'
+vim.o.clipboard = 'unnamedplus'
+vim.o.cursorline = true
+vim.o.ttyfast = true
+
+-- Python interpreter
+vim.g.python3_host_prog = '/Users/akirawu/miniforge3/bin/python3'
+
+-- Airline config
+vim.g.lightline = { colorscheme = 'gruvbox' }
+vim.g.airline_theme = 'base16'
+vim.cmd('colorscheme gruvbox')
+
+-- Auto-save settings
+vim.g.auto_save = 1
+vim.g.auto_save_events = {"InsertLeave", "TextChanged"}
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  callback = function()
+    -- NERDTree mappings
+    vim.api.nvim_set_keymap('n', '<leader>n', ':NERDTreeFocus<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<C-n>', ':NERDTree<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<C-t>', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
+  end
+})
+
+-- Window navigation mappings
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+  end
+})
+
+-- EasyMotion mappings
+vim.api.nvim_set_keymap('', '<Leader>', '<Plug>(easymotion-prefix)', {})
+vim.api.nvim_set_keymap('', '<Leader>w', '<Plug>(easymotion-bd-w)', {})
+
+-- Buffer management mappings
+vim.o.hidden = true
+vim.api.nvim_set_keymap('n', 'bl', ':ls<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'bn', ':bnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'bp', ':bprevious<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'bd', ':bdelete<CR>', { noremap = true, silent = true })
+
+-- Coc.nvim settings
+vim.api.nvim_set_keymap('i', '<TAB>', 'coc#pum#visible() ? coc#pum#next(1) : "<TAB>"', { noremap = true, expr = true })
+vim.api.nvim_set_keymap('i', '<S-TAB>', 'coc#pum#visible() ? coc#pum#prev(1) : "<C-h>"', { noremap = true, expr = true })
+vim.api.nvim_set_keymap('i', '<C-Space>', 'coc#refresh()', { noremap = true, silent = true, expr = true })
+vim.api.nvim_set_keymap('n', 'gd', '<Plug>(coc-definition)', { silent = true })
+vim.api.nvim_set_keymap('n', 'gy', '<Plug>(coc-type-definition)', { silent = true })
+vim.api.nvim_set_keymap('n', 'gi', '<Plug>(coc-implementation)', { silent = true })
+vim.api.nvim_set_keymap('n', 'gr', '<Plug>(coc-references)', { silent = true })
+
+-- Floaterm mappings
+vim.api.nvim_set_keymap('n', '<F7>', ':FloatermNew<CR>', { silent = true })
+vim.api.nvim_set_keymap('t', '<F7>', '<C-\\><C-n>:FloatermNew<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<F8>', ':FloatermPrev<CR>', { silent = true })
+vim.api.nvim_set_keymap('t', '<F8>', '<C-\\><C-n>:FloatermPrev<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<F9>', ':FloatermNext<CR>', { silent = true })
+vim.api.nvim_set_keymap('t', '<F9>', '<C-\\><C-n>:FloatermNext<CR>', { silent = true })
+vim.api.nvim_set_keymap('n', '<F12>', ':FloatermToggle<CR>', { silent = true })
+vim.api.nvim_set_keymap('t', '<F12>', '<C-\\><C-n>:FloatermToggle<CR>', { silent = true })
+
---
+++ plugin/packer_compiled.lua
@@ -0,0 +1,249 @@
+-- Automatically generated packer.nvim plugin loader code
+
+if vim.api.nvim_call_function('has', {'nvim-0.5'}) ~= 1 then
+  vim.api.nvim_command('echohl WarningMsg | echom "Invalid Neovim version for packer.nvim! | echohl None"')
+  return
+end
+
+vim.api.nvim_command('packadd packer.nvim')
+
+local no_errors, error_msg = pcall(function()
+
+_G._packer = _G._packer or {}
+_G._packer.inside_compile = true
+
+local time
+local profile_info
+local should_profile = false
+if should_profile then
+  local hrtime = vim.loop.hrtime
+  profile_info = {}
+  time = function(chunk, start)
+    if start then
+      profile_info[chunk] = hrtime()
+    else
+      profile_info[chunk] = (hrtime() - profile_info[chunk]) / 1e6
+    end
+  end
+else
+  time = function(chunk, start) end
+end
+
+local function save_profiles(threshold)
+  local sorted_times = {}
+  for chunk_name, time_taken in pairs(profile_info) do
+    sorted_times[#sorted_times + 1] = {chunk_name, time_taken}
+  end
+  table.sort(sorted_times, function(a, b) return a[2] > b[2] end)
+  local results = {}
+  for i, elem in ipairs(sorted_times) do
+    if not threshold or threshold and elem[2] > threshold then
+      results[i] = elem[1] .. ' took ' .. elem[2] .. 'ms'
+    end
+  end
+  if threshold then
+    table.insert(results, '(Only showing plugins that took longer than ' .. threshold .. ' ms ' .. 'to load)')
+  end
+
+  _G._packer.profile_output = results
+end
+
+time([[Luarocks path setup]], true)
+local package_path_str = "/Users/akirawuc/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/akirawuc/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/akirawuc/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/akirawuc/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/akirawuc/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+if not string.find(package.path, package_path_str, 1, true) then
+  package.path = package.path .. ';' .. package_path_str
+end
+
+if not string.find(package.cpath, install_cpath_pattern, 1, true) then
+  package.cpath = package.cpath .. ';' .. install_cpath_pattern
+end
+
+time([[Luarocks path setup]], false)
+time([[try_loadstring definition]], true)
+local function try_loadstring(s, component, name)
+  local success, result = pcall(loadstring(s), name, _G.packer_plugins[name])
+  if not success then
+    vim.schedule(function()
+      vim.api.nvim_notify('packer.nvim: Error running ' .. component .. ' for ' .. name .. ': ' .. result, vim.log.levels.ERROR, {})
+    end)
+  end
+  return result
+end
+
+time([[try_loadstring definition]], false)
+time([[Defining packer_plugins]], true)
+_G.packer_plugins = {
+  ["coc.nvim"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/coc.nvim",
+    url = "https://github.com/neoclide/coc.nvim"
+  },
+  ["copilot.vim"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/copilot.vim",
+    url = "https://github.com/github/copilot.vim"
+  },
+  ["eva01.vim"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/eva01.vim",
+    url = "https://github.com/hachy/eva01.vim"
+  },
+  gruvbox = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/gruvbox",
+    url = "https://github.com/morhetz/gruvbox"
+  },
+  ["html5.vim"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/html5.vim",
+    url = "https://github.com/othree/html5.vim"
+  },
+  ["jsonc.vim"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/jsonc.vim",
+    url = "https://github.com/neoclide/jsonc.vim"
+  },
+  ["lsp_signature.nvim"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/lsp_signature.nvim",
+    url = "https://github.com/ray-x/lsp_signature.nvim"
+  },
+  nerdtree = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/nerdtree",
+    url = "https://github.com/preservim/nerdtree"
+  },
+  ["nvim-web-devicons"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    url = "https://github.com/kyazdani42/nvim-web-devicons"
+  },
+  ["packer.nvim"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    url = "https://github.com/wbthomason/packer.nvim"
+  },
+  ["plenary.nvim"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    url = "https://github.com/nvim-lua/plenary.nvim"
+  },
+  ["rust.vim"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/rust.vim",
+    url = "https://github.com/rust-lang/rust.vim"
+  },
+  ["toggleterm.nvim"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/toggleterm.nvim",
+    url = "https://github.com/akinsho/toggleterm.nvim"
+  },
+  ["trouble.nvim"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/trouble.nvim",
+    url = "https://github.com/folke/trouble.nvim"
+  },
+  ["typescript-vim"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/typescript-vim",
+    url = "https://github.com/leafgarland/typescript-vim"
+  },
+  ["vim-abolish"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-abolish",
+    url = "https://github.com/tpope/vim-abolish"
+  },
+  ["vim-airline"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-airline",
+    url = "https://github.com/vim-airline/vim-airline"
+  },
+  ["vim-airline-themes"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-airline-themes",
+    url = "https://github.com/vim-airline/vim-airline-themes"
+  },
+  ["vim-auto-save"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-auto-save",
+    url = "https://github.com/907th/vim-auto-save"
+  },
+  ["vim-commentary"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-commentary",
+    url = "https://github.com/tpope/vim-commentary"
+  },
+  ["vim-devicons"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-devicons",
+    url = "https://github.com/ryanoasis/vim-devicons"
+  },
+  ["vim-easymotion"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-easymotion",
+    url = "https://github.com/easymotion/vim-easymotion"
+  },
+  ["vim-floaterm"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-floaterm",
+    url = "https://github.com/voldikss/vim-floaterm"
+  },
+  ["vim-javascript"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-javascript",
+    url = "https://github.com/pangloss/vim-javascript"
+  },
+  ["vim-jsx-pretty"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-jsx-pretty",
+    url = "https://github.com/MaxMEllon/vim-jsx-pretty"
+  },
+  ["vim-jsx-typescript"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-jsx-typescript",
+    url = "https://github.com/peitalin/vim-jsx-typescript"
+  },
+  ["vim-sensible"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-sensible",
+    url = "https://github.com/tpope/vim-sensible"
+  },
+  ["vim-solidity"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-solidity",
+    url = "https://github.com/TovarishFin/vim-solidity"
+  },
+  ["vim-startify"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-startify",
+    url = "https://github.com/mhinz/vim-startify"
+  },
+  ["vim-styled-components"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-styled-components",
+    url = "https://github.com/styled-components/vim-styled-components"
+  },
+  ["vim-surround"] = {
+    loaded = true,
+    path = "/Users/akirawuc/.local/share/nvim/site/pack/packer/start/vim-surround",
+    url = "https://github.com/tpope/vim-surround"
+  }
+}
+
+time([[Defining packer_plugins]], false)
+
+_G._packer.inside_compile = false
+if _G._packer.needs_bufread == true then
+  vim.cmd("doautocmd BufRead")
+end
+_G._packer.needs_bufread = false
+
+if should_profile then save_profiles() end
+
+end)
+
+if not no_errors then
+  error_msg = error_msg:gsub('"', '\\"')
+  vim.api.nvim_command('echohl ErrorMsg | echom "Error in packer_compiled: '..error_msg..'" | echom "Please check your config for correctness" | echohl None')
+end
